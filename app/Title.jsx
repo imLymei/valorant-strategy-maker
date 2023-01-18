5cbf47015de19ab2bdbec444363b0a59fd5a9bff
Lymei <ronaldofelipespm@bol.com.br>
2023-01-18 02:16:53 -0300

    Title update

--- app/Title.jsx
+++ app/Title.jsx
@@ -5,7 +5,7 @@ function Title() {
 		<div className='text-center text-[#e9e9e9] text-4xl p-8'>
 			<h1>
 				<span className='text-red-500 font-semibold'>VALORANT</span>:{' '}
-				<span className='text-pink-500'>Planejador</span> de <span className='text-blue-500'>Tática</span>
+				<span className='text-pink-500'>Criador</span> de <span className='text-blue-500'>Tática</span>
 			</h1>
 		</div>
 	);
